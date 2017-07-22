5d6c383ac2cccf142318316ab8cdce1ca9b2abf7
Huw Pritchard <huw008@gmail.com>
2017-07-22 20:06:22 +0100

    Fix prolog config
    Former-commit-id: 27623b687afe9cca4ce1b149d22d717f0e2ef053
    Former-commit-id: e08cec06ab9fc56f2086420efd27b958e4574101

--- source/tokens/tokens.cpp
+++ source/tokens/tokens.cpp
@@ -1,56 +1,85 @@
-#include "tokens.h"
+/*
+    HuwInterpreter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-Tokens::Tokens()
+    HuwInterpreter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HuwInterpreter.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "tokendetector.h"
+
+void TokenDetector::init()
 {
-    init();
+
 }
 
-void Tokens::init()
+TokenDetector::TokenDetector()
 {
-    items.clear();
-    items["("] = LEFTARENTHESIS;
-    items["*"] = MULTIPLICATION;
-    items["/"] = DIVISION;
-    items["+"] = ADDITION;
-    items["-"] = SUBTRACTION;
-    items["%"] = MOD;
-    items["="] = EQUALS;
-    items["=="] = IFEQUALS;
-    items["!="] = IFNOTEQUALS;
-    items["<"] = IFLESSTHAN;
-    items["<="] = IFLESSTHANOREQUAL;
-    items[">"] = IFGREATER;
-    items[">="] = IFGREATERTHANOREQUAL;
-    items["{"] = LEFTBRACKET;
-    items["}"] = RIGHTBRACKET;
-    items[";"] = SEMICOLON;
-    items[","] = COMMA;
-    items["&&"] = AND;
-    items["||"] = OR;
-    items["."] = DOT;
-    items["*="] = MULTIPLICATIONEQUAL;
-    items["/="] = DIVISIONEQUAL;
-    items["+="] = ADDITIONEQUAL;
-    items["-="] = SUBTRACTIONEQUAL;
-    items["++"] = INCREMENT;
-    items["--"] = DECREMENT;
+    types[")"] = TokenType::RIGHTPARENTHESIS;
+    types["("] = TokenType::LEFTARENTHESIS;
+    types["*"] = TokenType::MULTIPLICATION;
+    types["/"] = TokenType::DIVISION;
+    types["+"] = TokenType::ADDITION;
+    types["-"] = TokenType::SUBTRACTION;
+    types["%"] = TokenType::MOD;
+    types["="] = TokenType::EQUALS;
+    types["=="] = TokenType::IFEQUALS;
+    types["!="] = TokenType::IFNOTEQUALS;
+    types["<"] = TokenType::IFLESSTHAN;
+    types["<="] = TokenType::IFLESSTHANOREQUAL;
+    types[">"] = TokenType::IFGREATER;
+    types[">="] = TokenType::IFGREATERTHANOREQUAL;
+    types["{"] = TokenType::LEFTBRACKET;
+    types["}"] = TokenType::RIGHTBRACKET;
+    types[";"] = TokenType::SEMICOLON;
+    types[","] = TokenType::COMMA;
+    types["\""] = TokenType::QUOTE;
+    types["."] = TokenType::DOT;
+    types["!"] = TokenType::NOT;
 }
 
-bool Tokens::exists(TokenType tokenType)
+std::string TokenDetector::getString(TokenType token)
 {
-    return std::find_if(items.begin(), items.end(),
-        [tokenType](const std::unordered_map<std::string, TokenType>::value_type& item) { return item.second == tokenType; }) != items.end();
-}
-
-bool Tokens::exists(std::string text)
-{
-    return items.find(text) != items.end();
-}
-
-void Tokens::add(TokenType tokenType, std::string text)
-{
-    if (!exists(tokenType))
+    for (std::unordered_map<std::string, TokenType>::iterator it = types.begin(); it != types.end(); ++it)
     {
-        items[text] = tokenType;
+        if (it->second == token)
+        {
+            return it->first;
+        }
     }
+    return "";
+}
+
+TokenType TokenDetector::getToken(std::string value)
+{
+    return types[value];
+}
+
+bool TokenDetector::compare(std::string value1, TokenType value2)
+{
+    std::unordered_map<std::string,TokenType>::const_iterator got = types.find(value1);
+    if (got != types.end())
+    {
+        if (got->second == value2)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool TokenDetector::compare(char value1, TokenType value2)
+{
+    std::stringstream ss;
+    std::string temp;
+    ss << value1;
+    ss >> temp;
+    return compare(temp, value2);
 }
