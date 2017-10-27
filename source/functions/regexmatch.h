952b40ceb06e4655f893c781fb213c838be181ff
Huw Pritchard <huw008@gmail.com>
2017-10-27 14:58:59 +0100

    Added regex 3 funcitons
    Former-commit-id: b785c383402e4b3c37bb1073d29fc6c2f4a1585f
    Former-commit-id: 97fd09476c263a45944cafa826d0e844b8956252

---
+++ source/functions/regexmatch.h
@@ -0,0 +1,32 @@
+/*
+    HuwInterpreter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HuwInterpreter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HuwInterpreter.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef REGEXMATCH_H
+#define REGEXMATCH_H
+
+#include "function.h"
+#include "../variables/hashtablevariable.h"
+#include <regex>
+
+class RegexMatch : public Function
+{
+public:
+    RegexMatch(std::shared_ptr<Passable> passable);
+    std::shared_ptr<Variable> run(std::shared_ptr<Token> token,
+                                  std::shared_ptr<Scope> scope,
+                                  std::vector<std::shared_ptr<Node>> variables);
+};
+
+#endif // REGEXMATCH_H
