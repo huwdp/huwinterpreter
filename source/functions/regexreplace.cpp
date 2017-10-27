6d164760f1c29f5fe652b4e9b412176fdf254320
Huw Pritchard <huw008@gmail.com>
2017-10-27 14:58:59 +0100

    Added regex 3 funcitons
    Former-commit-id: 4efa7b225293e1cc59abf4159b8a42917cae2850 [formerly 16776cbbe070fa56b0b286f53b131471d4143777]
    Former-commit-id: 97fd09476c263a45944cafa826d0e844b8956252
    Former-commit-id: ddee5be0ff43ea6ed385fa7da95d318dd657e3b1

---
+++ source/functions/regexreplace.cpp
@@ -0,0 +1,62 @@
+/*
+    HuwInterpreter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HuwInterpreter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HuwInterpreter.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "regexreplace.h"
+
+RegexReplace::RegexReplace(std::shared_ptr<Passable> passable)
+    : Function(passable)
+{
+    name = "regexReplace";
+}
+
+std::shared_ptr<Variable> RegexReplace::run(std::shared_ptr<Token> token,
+                              std::shared_ptr<Scope> scope,
+                              std::vector<std::shared_ptr<Node>> variables)
+{
+    std::shared_ptr<Variable> returnNode;
+    if (variables.size() == 3)
+    {
+        std::shared_ptr<Node> node1 = variables.at(0);
+        std::shared_ptr<Node> node2 = variables.at(1);
+        std::shared_ptr<Node> node3 = variables.at(2);
+        if (node1 == nullptr || node2 == nullptr || node3 == nullptr)
+        {
+           passable->errors->add(passable->errorFactory->invalidArgument(RUNTIME_ERROR, token, name));
+           return null;
+        }
+
+        std::shared_ptr<Variable> var1 = node1->execute(scope);
+        std::shared_ptr<Variable> var2 = node2->execute(scope);
+        std::shared_ptr<Variable> var3 = node3->execute(scope);
+
+        if (var1 == nullptr || var2 == nullptr || var3 == nullptr)
+        {
+           passable->errors->add(passable->errorFactory->invalidArgument(RUNTIME_ERROR, token, name));
+           return null;
+        }
+
+        std::string str = var1->toString();
+        std::string str2 = var2->toString();
+        std::string str3 = var3->toString();
+        std::regex arg2(str2);
+        std::string result = std::regex_replace(str, arg2, str3);
+        return std::make_shared<StringVariable>(passable, result);
+    }
+    else
+    {
+       passable->errors->add(passable->errorFactory->requiresArguments(token, name, "", 2));
+    }
+    return returnNode;
+}
---
+++ source/functions/regexsearch.cpp
@@ -0,0 +1,61 @@
+/*
+    HuwInterpreter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HuwInterpreter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HuwInterpreter.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "regexsearch.h"
+
+RegexSearch::RegexSearch(std::shared_ptr<Passable> passable)
+    : Function(passable)
+{
+    name = "regexSearch";
+}
+
+std::shared_ptr<Variable> RegexSearch::run(std::shared_ptr<Token> token,
+                              std::shared_ptr<Scope> scope,
+                              std::vector<std::shared_ptr<Node>> variables)
+{
+    std::shared_ptr<Variable> returnNode;
+    if (variables.size() == 2)
+    {
+        std::shared_ptr<Node> node1 = variables.at(0);
+        std::shared_ptr<Node> node2 = variables.at(1);
+        if (node1 == nullptr || node2 == nullptr)
+        {
+            passable->errors->add(passable->errorFactory->invalidArgument(RUNTIME_ERROR, token, name));
+            return null;
+        }
+
+        std::shared_ptr<Variable> var1 = node1->execute(scope);
+        std::shared_ptr<Variable> var2 = node2->execute(scope);
+
+        if (var1 == nullptr || var2 == nullptr)
+        {
+            passable->errors->add(passable->errorFactory->invalidArgument(RUNTIME_ERROR, token, name));
+            return null;
+        }
+
+        std::string str = var1->toString();
+        std::string str2 = var2->toString();
+        std::regex regex(str2);
+        bool result = std::regex_search(str, regex);
+        return std::make_shared<NumberVariable>(passable, result);
+    }
+    else
+    {
+        passable->errors->add(passable->errorFactory->requiresArguments(token, name, "", 2));
+    }
+    return returnNode;
+
+
+}
