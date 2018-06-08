8b133d873224a0e552c61d87f9fb6cf301f27d21
Huw Pritchard <huw008@gmail.com>
2018-06-09 00:03:39 +0100

    Added clock function, current time function and localtime function
    Former-commit-id: 130379c18752325b580e1cb2d55f6e50cdbe973c [formerly 83d8a93fb15cf8cbff1e05654bb7caa81523745b]
    Former-commit-id: a717a4399147c9231770f0197db64677a528e94b
    Former-commit-id: a9f4a68e3b857f073a98bff3831b0950c593a3c5

---
+++ source/functions/localtimefunction.cpp
@@ -0,0 +1,79 @@
+#include "localtimefunction.h"
+
+/*
+    HuwInterpreter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HuwInterpreter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HuwInterpreter.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "localtimefunction.h"
+
+namespace HuwInterpreter {
+    namespace Functions {
+        LocalTimeFunction::LocalTimeFunction(std::shared_ptr<Passable> passable)
+            : Function(passable)
+        {
+            setName("localTime");
+        }
+
+        std::shared_ptr<Variable> LocalTimeFunction::execute(std::shared_ptr<Tokens::Token> token, std::shared_ptr<Scope> globalScope,
+                                           std::shared_ptr<Scope> scope,
+                                           std::vector<std::shared_ptr<Nodes::Node>> arguments)
+        {
+            if (arguments.size()== 0)
+            {
+                passable->getErrorManager()->add(passable->getErrorFactory()->requiresAtLeastXArguments(token, getName(), 1));
+                return nullVariable;
+            }
+            if (arguments.size() > 2)
+            {
+                passable->getErrorManager()->add(passable->getErrorFactory()->requiresAtLeastXArguments(token, getName(), 1));
+                return nullVariable;
+            }
+
+            std::shared_ptr<Nodes::Node> arg1 = arguments.at(0);
+
+            if (arg1 == nullptr)
+            {
+                passable->getErrorManager()->add(passable->getErrorFactory()->invalidArgument(token, RUNTIME_ERROR, arg1->getName()));
+                return nullVariable;
+            }
+
+            std::shared_ptr<Variable> argVar1 = arg1->execute(globalScope, scope);
+            long int value = argVar1->toInt();
+            if (arguments.size() == 2)
+            {
+                std::shared_ptr<Nodes::Node> arg2 = arguments.at(1);
+
+                if (arg2 == nullptr)
+                {
+                    passable->getErrorManager()->add(passable->getErrorFactory()->invalidArgument(token, RUNTIME_ERROR, arg1->getName()));
+                    return nullVariable;
+                }
+
+                std::shared_ptr<Variable> argVar2 = arg2->execute(globalScope, scope);
+
+                if (argVar2 == nullptr)
+                {
+                    passable->getErrorManager()->add(passable->getErrorFactory()->invalidArgument(token, RUNTIME_ERROR, arg2->getName()));
+                    return nullVariable;
+                }
+                std::ostringstream output;
+                output << std::put_time(std::localtime(&value),argVar2->toString().c_str());
+                return std::make_shared<StringVariable>(passable,output.str());
+            }
+            std::ostringstream output;
+            output << std::put_time(std::localtime(&value),"%c %Z");    // UTC
+            return std::make_shared<StringVariable>(passable,output.str());
+        }
+    }
+}
