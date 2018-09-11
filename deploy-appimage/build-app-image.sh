cd56700a47df95ce0f1b931c2532cf4eeb772228
Huw Pritchard <huw008@gmail.com>
2018-09-11 21:38:25 +0100

    Changed build AppArmour image file to copy bin instead of move
    Former-commit-id: f7cc65ed107cd7c7e6f7a27fb253878d6813b144 [formerly 1a667e6429450b8113d5d557df39a15e8bf50dce]
    Former-commit-id: 8d159c715568fc3c209d596369148817fcbf2759
    Former-commit-id: 55cb5bf0d596064057a95d051c56e5069b9d4b26

--- deploy-appimage/build-app-image.sh
+++ deploy-appimage/build-app-image.sh
@@ -2,7 +2,5 @@
 make -C ../build-HuwInterpreter-Desktop-Release/
 cp ../build-HuwInterpreter-Desktop-Release/HuwInterpreter huwcode/bin/huwcode
 ./linuxdeployqt-continuous-x86_64.AppImage huwcode/share/applications/huwcode.desktop -bundle-non-qt-libs
-#mv dist.AppImage huwcode-linux-x86_64.AppImage
-#export HuwCode=huwcode-linux-x86_64.AppImage
 dt=`date '+%d%m%Y%H%M%S'`
-mv AppRun "releases/huwcode$dt"
+cp AppRun "releases/huwcode$dt.AppImage"
