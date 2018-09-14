4f4342dfaca4dd418c674feb4506a2d8e88b1a0f
Huw Pritchard <huw008@gmail.com>
2018-09-14 22:15:00 +0100

    Fixed AppImage build script
    Former-commit-id: 43da6afc028386e1182733e6f51a87fead97dbdc
    Former-commit-id: 7c1b8b8007c021afd77044c8bd7cf956738e1514

--- deploy-appimage/build-app-image.sh
+++ deploy-appimage/build-app-image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 make -C ../build-HuwInterpreter-Desktop-Release/
 cp ../build-HuwInterpreter-Desktop-Release/HuwInterpreter huwcode/bin/huwcode
-./linuxdeployqt-continuous-x86_64.AppImage huwcode/share/applications/huwcode.desktop -bundle-non-qt-libs
-dt=`date '+%d%m%Y%H%M%S'`
-cp AppRun "releases/huwcode$dt.AppImage"
+./linuxdeployqt-continuous-x86_64.AppImage huwcode/share/applications/huwcode.desktop -appimage -bundle-non-qt-libs -verbose=2
+dt=`date '+%Y%m%d'`
+cp HuwCode-x86_64.AppImage "releases/huwcode$dt.AppImage"
+chmod a+x "releases/huwcode$dt.AppImage"
