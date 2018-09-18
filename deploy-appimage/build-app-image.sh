68be1827ff19f6ab03ba76e1b779cb73093951c4
Travis CI <travis@travis-ci.org>
2018-09-18 23:14:00 +0100

    Updated build script
    Former-commit-id: d5036dfab413082e8a008026d301255c5d1d6f5e
    Former-commit-id: f2fab78f1b1f715aebc96f9cef2322f2e90e6f80
    Former-commit-id: 4256c9ecacb3d3545e8bb024e470704871ac0688

--- deploy-appimage/build-app-image.sh
+++ deploy-appimage/build-app-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 make -C ../build-HuwInterpreter-Desktop-Release/
 cp ../build-HuwInterpreter-Desktop-Release/HuwInterpreter huwcode/bin/huwcode
+export VERSION=$(date '+%Y%m%d') # linuxdeployqt uses this for naming the file
 ./linuxdeployqt-continuous-x86_64.AppImage huwcode/share/applications/huwcode.desktop -appimage -bundle-non-qt-libs -verbose=2
-dt=`date '+%Y%m%d'`
-cp HuwCode-x86_64.AppImage "releases/huwcode$dt.AppImage"
-chmod a+x "releases/huwcode$dt.AppImage"
+#cp HuwCode-x86_64.AppImage "releases/huwcode$dt.AppImage"
+#chmod a+x "releases/huwcode$dt.AppImage"
