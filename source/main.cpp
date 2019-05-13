b55614b88a8582d2957da851751d9c475e5ff48a
huwdp <huw008@gmail.com>
2019-05-14 00:06:14 +0100

    Added --help to default output screen
    Former-commit-id: 654b7fcc6e9dfa4274a34d3c5c2a4f2c0056ed72
    Former-commit-id: 1886645e1872038a5a988763222221b3f9387897

--- source/main.cpp
+++ source/main.cpp
@@ -31,14 +31,14 @@ void printHelp(int argc, char* argv[])
         std::cerr << "1. Usage: " << argv[0] << " [filename] to run file." << std::endl;
         std::cerr << "2. Usage: " << argv[0] << " -t [text] to run text." << std::endl;
         std::cerr << "3. Usage: " << argv[0] << " --hc [filename] for HuwCode formatter" << std::endl;
-        std::cerr << "4. Usage: " << argv[0] << " -h for help." << std::endl;
+        std::cerr << "4. Usage: " << argv[0] << " -h or --help for help." << std::endl;
     }
     else
     {
         std::cerr << "1. Usage: [filename] to run file." << std::endl;
         std::cerr << "2. Usage: -t [text] to run text." << std::endl;
         std::cerr << "3. Usage: --hc for HuwCode formatter" << std::endl;
-        std::cerr << "4. Usage: -h for help." << std::endl;
+        std::cerr << "4. Usage: -h or --help for help." << std::endl;
     }
 }
 
