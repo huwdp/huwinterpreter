1dc5f9a5279b8bd98b285367a3abf6746af6a0ce
huwdp <huw008@gmail.com>
2019-07-14 22:56:53 +0100

    Added sleep function

---
+++ source/functions/sleepfunction.cpp
@@ -0,0 +1,58 @@
+/*
+    HuwInterpreter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HuwInterpreter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HuwInterpreter.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "sleepfunction.h"
+
+namespace HuwInterpreter {
+    namespace Functions {
+        SleepFunction::SleepFunction(std::shared_ptr<Passable> passable)
+            : Function(passable)
+        {
+            setName("sleep");
+        }
+
+        std::shared_ptr<Variable> SleepFunction::execute(std::shared_ptr<Tokens::Token> token, std::shared_ptr<Scope> globalScope,
+                                             std::shared_ptr<Scope> scope,
+                                             std::vector<std::shared_ptr<Nodes::Node>> arguments)
+        {
+            if (arguments.size() == 1)
+            {
+                std::shared_ptr<Nodes::Node> node = arguments.at(0);
+                if (node == nullptr)
+                {
+                    passable->getErrorManager()->add(passable->getErrorFactory()->invalidArgument(token, RUNTIME_ERROR, name));
+                    return nullVariable;
+                }
+                std::shared_ptr<Variable> var = node->execute(globalScope, scope);
+                if (var == nullptr)
+                {
+                    passable->getErrorManager()->add(passable->getErrorFactory()->invalidArgument(token, RUNTIME_ERROR, name));
+                    return nullVariable;
+                }
+                if (var->isNumber())
+                {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(var->toInt()));
+                }
+                else
+                {
+                    passable->getErrorManager()->add(passable->getErrorFactory()->argumentMustBeNumberType(token, name));
+                }
+                return nullVariable;
+            }
+            passable->getErrorManager()->add(passable->getErrorFactory()->requiresArguments(token, name, "", 1));
+            return nullVariable;
+        }
+    }
+}
