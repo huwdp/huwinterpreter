beb6c92ce71c644b03c633b1456ffde21054f2b0
huwdp <huw008@gmail.com>
2019-07-14 22:17:49 +0100

    Initial implementation of HTTP support
    Former-commit-id: 35982c31240a342f15189f57c46d2a431c7ef5ab [formerly 18ed2ab36a2ea9009acd9603a3e482481a2b65ff [formerly 6ce5661857e7bd699e1fab6bea6538d671906caf]]
    Former-commit-id: 166ce02a37308b8b89e83c8c903ef2312c027656
    Former-commit-id: a955dcb0450e949722d137d63fb0c8a6327f6115

---
+++ source/functions/httpheadfunction.h
@@ -0,0 +1,40 @@
+/*
+    HuwInterpreter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HuwInterpreter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HuwInterpreter.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef HTTPHEADFUNCTION_H
+#define HTTPHEADFUNCTION_H
+
+#include "function.h"
+#include <curl/curl.h>
+#include "variables/doublevariable.h"
+#include "variables/stringvariable.h"
+#include "variables/hashtablevariable.h"
+
+namespace HuwInterpreter {
+    namespace Functions {
+        class HttpHeadFunction : public Function
+        {
+        public:
+            HttpHeadFunction(std::shared_ptr<Passable> passable);
+            std::shared_ptr<Variable> execute(std::shared_ptr<Tokens::Token> token,
+                                          std::shared_ptr<Scope> globalScope,
+                                          std::shared_ptr<Scope> scope,
+                                          std::vector<std::shared_ptr<Nodes::Node>> arguments);
+        };
+    }
+}
+
+#endif // HTTPHEADFUNCTION_H
